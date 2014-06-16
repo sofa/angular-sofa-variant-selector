ceac2a4513aeaeb98bd79cf09afb0c348a362d70
Ingo Blenke <info@ingoblenke.de>
2014-06-16 15:40:13 +0200

    feat(cc-variant-selector): make variant selector use the cc-select-box

--- src/directives/ccVariantSelector/ccVariantSelector.js
+++ src/directives/ccVariantSelector/ccVariantSelector.js
@@ -1,19 +1,20 @@
-angular.module('sdk.directives.ccVariantSelector', ['src/directives/ccVariantSelector/ccvariantselector.tpl.html']);
+angular.module('sdk.directives.ccVariantSelector', ['src/directives/ccVariantSelector/ccvariantselector.tpl.html', 'sdk.directives.ccSelectBox']);
 
 angular.module('sdk.directives.ccVariantSelector')
-    .filter('ccVariantFilter', ['$filter', function($filter) {
+    .filter('ccVariantFilter', ['$filter', function ($filter) {
 
         'use strict';
 
-        return function(values, selectedValues, key) {
+        // variants, selectedProperties, propertyKey
+        return function (values, selectedValues, key) {
             var selected = {},
                 applyFilters = false;
 
             // reformat for built in filter and exclude current property
-            for (var property in selectedValues){
-                if (key!==property && selectedValues[property]!==null && selectedValues[property]!==undefined) {
-                   selected['properties.' + property] = selectedValues[property];
-                   applyFilters = true;
+            for (var property in selectedValues) {
+                if (key !== property && selectedValues[property] !== null && selectedValues[property] !== undefined) {
+                    selected['properties.' + property] = selectedValues[property];
+                    applyFilters = true;
                 }
             }
 
@@ -22,16 +23,17 @@ angular.module('sdk.directives.ccVariantSelector')
 
             // extract flat values for the curent property
             var result = [];
-            variants.forEach(function(variant){
-                if (result.indexOf(variant.properties[key]) === -1 && variant.stock > 0){
+            variants.forEach(function (variant) {
+                if (result.indexOf(variant.properties[key]) === -1 && variant.stock > 0) {
                     result.push(variant.properties[key]);
                 }
             });
+
             return result;
         };
     }])
 
-    .directive('ccVariantSelector', function() {
+    .directive('ccVariantSelector', function ($filter) {
 
         'use strict';
 
@@ -40,35 +42,33 @@ angular.module('sdk.directives.ccVariantSelector')
             replace: true,
             scope: {
                 variants: '=',
-                variant : '=?',
+                variant: '=?',
                 selectedProperties: '=?',
                 chooseText: '=?'
             },
             templateUrl: 'src/directives/ccVariantSelector/ccvariantselector.tpl.html',
-            link: function(scope, element, attrs){
+            link: function (scope) {
 
                 // extract flat list of available properties
                 // maybe iterating on the first variant is enough ?
                 scope.properties = [];
-                scope.selectedProperties = {};
+                scope.selectedProperties = scope.selectedProperties || {};
+                scope.data = {};
 
-                scope.variants.forEach(function(variant){
-                    for (var property in variant.properties){
-                        //create a placeholder value on the selectedProperties hash
-                        //for each available property. So we can later figure out
-                        //which are missing.
-                        scope.selectedProperties[property] = null;
-                        if (scope.properties.indexOf(property) === -1){
-                            scope.properties.push(property);
-                        }
-                    }
-                });
-                
+                var getDataByProperty = function (property) {
+                    return $filter('ccVariantFilter')(scope.variants, scope.selectedProperties, property);
+                };
 
-                var findVariant = function(variants, selectedProperties){
-                    var filteredVariants = variants.filter(function(variant){
-                        for (var property in variant.properties){
-                            if (variant.properties[property] !== selectedProperties[property]){
+                var setData = function () {
+                    scope.properties.forEach(function (property) {
+                        scope.data[property] = getDataByProperty(property);
+                    });
+                };
+
+                var findVariant = function (variants, selectedProperties) {
+                    var filteredVariants = variants.filter(function (variant) {
+                        for (var property in variant.properties) {
+                            if (variant.properties[property] !== selectedProperties[property]) {
                                 return false;
                             }
                         }
@@ -79,9 +79,21 @@ angular.module('sdk.directives.ccVariantSelector')
                     return filteredVariants.length > 0 ? filteredVariants[0] : null;
                 };
 
-                scope.$watch('selectedProperties', function(val){
-                    var variant = findVariant(scope.variants, val);
-                    scope.variant = variant;
+                scope.variants.forEach(function (variant) {
+                    for (var property in variant.properties) {
+                        //create a placeholder value on the selectedProperties hash
+                        //for each available property. So we can later figure out
+                        //which are missing.
+                        scope.selectedProperties[property] = null;
+                        if (scope.properties.indexOf(property) === -1) {
+                            scope.properties.push(property);
+                        }
+                    }
+                });
+
+                scope.$watch('selectedProperties', function (newVal) {
+                    scope.variant = findVariant(scope.variants, newVal);
+                    setData();
                 }, true);
             }
         };
