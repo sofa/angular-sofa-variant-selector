2cc8e9a73c27905d26a16c510416d73dda54303e
Ingo Blenke <info@ingoblenke.de>
2014-09-01 14:21:57 +0200

    chore(*): added dist files

--- dist/cc.angular.js
+++ dist/cc.angular.js
@@ -2549,7 +2549,8 @@ angular.module('sdk.directives', [
     'sdk.directives.sofaRadioButton',
     'sdk.directives.sofaTouchSlider',
     'sdk.directives.sofaFullPageView',
-    'sdk.directives.sofaImageZoom'
+    'sdk.directives.sofaImageZoom',
+    'sdk.directives.sofaImageAspectRatio'
 ]);
 angular.module('sdk.directives.sofaFullPageView', ['src/directives/sofaFullPageView/sofa-full-page-view.tpl.html']);
 
@@ -2617,6 +2618,36 @@ angular.module('sdk.directives.sofaFullPageView')
             require: '^sofaFullPageView',
             link: function ($scope, $element) {
                 $scope.originalElement = $element;
+            }
+        };
+    });
+angular.module('sdk.directives.sofaImageAspectRatio', [])
+    .directive('sofaImageAspectRatio', function ($window, $rootScope, deviceService) {
+
+        'use strict';
+        
+        $window.addEventListener('orientationchange', function () {
+            $rootScope.$emit('sofaImageAspectRatio.orientationChange');
+        }, false);
+
+        return {
+            restrict: 'A',
+            link: function ($scope, $element) {
+                var el = $element[0];
+
+                var setStyles = function () {
+                    if (deviceService.isInPortraitMode()) {
+                        el.style.maxWidth  = '100%';
+                        el.style.maxHeight = 'none';
+                    } else {
+                        el.style.maxWidth  = 'none';
+                        el.style.maxHeight = '100%';
+                    }
+                };
+
+                setStyles();
+
+                $scope.$onRootScope('sofaImageAspectRatio.orientationChange', setStyles);
             }
         };
     });
