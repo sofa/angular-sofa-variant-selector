a6e548edc5404a45627fb077d7df5fa96671ed1c
Ingo Blenke <info@ingoblenke.de>
2014-09-10 16:54:04 +0200

    feat(cc-check-box): make it work with form controller and array models

--- src/directives/ccCheckBox/ccCheckBox.js
+++ src/directives/ccCheckBox/ccCheckBox.js
@@ -1,8 +1,7 @@
-
 angular.module('sdk.directives.ccCheckBox', ['src/directives/ccCheckBox/cc-checkbox.tpl.html']);
 
 angular.module('sdk.directives.ccCheckBox')
-    .directive('ccCheckBox', function() {
+    .directive('ccCheckBox', function () {
 
         'use strict';
 
@@ -12,20 +11,54 @@ angular.module('sdk.directives.ccCheckBox')
             restrict: 'E',
             replace: true,
             scope: {
-                label: '=?',
-                value: '=?'
+                model: '=',
+                label: '=',
+                value: '=',
+                disabled: '=?',
+                propertyName: '@'
             },
             templateUrl: 'src/directives/ccCheckBox/cc-checkbox.tpl.html',
-            controller: ['$scope', function($scope) {
+            controller: ['$scope', function ($scope) {
                 return {
-                    getId: function(){
+                    getId: function () {
                         return $scope.id;
                     }
                 };
             }],
-            link: function(scope, $element, attrs){
+            link: function (scope) {
                 instanceCount++;
                 scope.id = instanceCount;
+
+                var isArrayData = angular.isArray(scope.model);
+
+                scope.innerModel = isArrayData ? '' : scope.model;
+
+                // In case label comes in as a number, which doesn't work with the html parser
+                scope.label = scope.label + '';
+
+                if (isArrayData) {
+                    // Changing the innerModel should change the outer model
+                    scope.$watch('innerModel', function (nv, ov) {
+                        if (nv !== ov) {
+                            var i = scope.model.indexOf(scope.value);
+
+                            if (nv === true && i === -1) {
+                                scope.model.push(scope.value);
+                            } else if (!nv && i > -1) {
+                                scope.model.splice(i, 1);
+                            }
+                        }
+                    });
+                    // Changes in the outer model must be reflected in the innerModel
+                    scope.$watch('model', function (nv) {
+                        var i = nv.indexOf(scope.value);
+                        scope.innerModel = i > -1;
+                    }, true);
+                } else {
+                    scope.$watch('innerModel', function (nv) {
+                        scope.model = nv;
+                    });
+                }
             }
         };
     });
