0fdbaa17560c71385c69514a1d1f825e32253654
Ingo Blenke <info@ingoblenke.de>
2014-11-26 15:30:14 +0100

    feat(checkout-service): added DI for storage and user services

--- src/services/CheckoutService.js
+++ src/services/CheckoutService.js
@@ -1,9 +1,17 @@
-angular.module('sdk.services.checkoutService', ['sdk.services.basketService', 'sdk.services.loggingService']);
+angular.module('sdk.services.checkoutService', [
+    'sdk.services.basketService',
+    'sdk.services.loggingService',
+    store.enabled ? 'sdk.services.localStorageService' : 'sdk.services.memoryStorageService',
+    'sdk.services.userService'
+]);
 
 angular
     .module('sdk.services.checkoutService')
-    .factory('checkoutService', ['$http', '$q', 'basketService', 'loggingService', 'configService', function($http, $q, basketService, loggingService, configService){
-        return new cc.CheckoutService($http, $q, basketService, loggingService, configService);
-}]);
+    .factory('checkoutService', [
+        '$http', '$q', 'basketService', 'loggingService', 'configService', 'storageService', 'userService',
+        function ($http, $q, basketService, loggingService, configService, storageService, userService) {
+            return new cc.CheckoutService($http, $q, basketService, loggingService, configService, storageService, userService);
+        }
+    ]);
 
 
